5893924da56bee4e44427b4c900113ac593d5714
dlesesne23 <davidplesesne@gmail.com>
2024-06-09 16:29:46 -0500

    Added new components for more personalization

--- frontend/Water-Ways/src/pages/HomePage.js
+++ frontend/Water-Ways/src/pages/HomePage.js
@@ -1,359 +1,61 @@
-import { StyleSheet, Text, View, Dimensions, ScrollView, FlatList, TouchableOpacity } from 'react-native'
-import { Icon } from 'react-native-elements'
-import { StatusBar } from 'expo-status-bar'
-import MapView, { PROVIDER_GOOGLE,} from 'react-native-maps'
-import * as Location from 'expo-location'
-import React, { useState, useRef, useEffect } from 'react'
+import React from "react";
+import { View, SafeAreaView, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavFavourites from "../components/NavFavourites";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
+import { GOOGLE_MAPS_APIKEY } from "@env";
+import NavOptions from "../components/NavOptions";
 
-const SCREEN_WIDTH = Dimensions.get('window').width
-
-import { colors, parameters } from '../global/styles'
-import { mapStyle } from '../global/map'
-
-const HomePage = ({navigation}) => {
-
-const [latlng, setLatLng] = useState({})
-
-const checkPermission = async () => {
-    const hasPermission = await Location.requestForegroundPermissionsAsync()
-    if (hasPermission.status === 'granted') {
-        const permission = await askPermission()
-        return permission
-    }
-}
-
-const askPermission = async () => {
-    const permission = await Location.requestForegroundPermissionsAsync()
-    return permission.status === 'granted'
-}
-
-const getLocation = async() => {
-    try {
-        const { granted } = await Location.requestForegroundPermissionsAsync()
-        if(!granted) return
-        const {
-            coords:{latitude, longitude},
-        } = await Location.getCurrentPositionAsync()
-        setLatLng({latitude:latitude, longitude:longitude})
-    } catch(err) {}
-}
-
-const _map = useRef(1)
-
-useEffect(() => {
-    checkPermission()
-    getLocation()
-    console.log(latlng)
-,[]})
+const HomePage = () => {
+  const dispatch = useDispatch();
 
   return (
-    <View style = {styles.container}>
-      <View style = {styles.header}>
-        <View style = {styles.icon1}>
-            <Icon type = "material-community"
-                  name = "menu"
-                  color = {colors.white}
-                  size = {40}  
-             />
-        </View>
-      </View>
-      <ScrollView bounces = {false}>
-        <View style = {styles.home}>
-            <Text style = {styles.text1}>Take the Scenic Route!</Text>
-            <View style = {styles.view1}>
-                <View style = {styles.view8}>
-                    <Text style = {styles.text2}>Want a change from your normal commute around your community. Take the Water Way and enjoy a nice boat ride to the boardwalk, a restaurant, or to your friends house!</Text>
-                    <TouchableOpacity onPress ={() => {navigation.navigate("RidePage")}}>
-                        <View style = {styles.button1}>
-                            <Text style = {styles.button1Text}>Ride</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </View>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{ uri: "https://links.papareact.com/gzs" }}
+        />
 
-        <View>
-            <FlatList 
-                numRows ={4}
-                horizontal = {true}
-                showsHorizontalScrollIndicator ={false}
-                data ={filterData}
-                keyExtractor = {(item)=>item.id}
-                renderItem = { ({item})=>(
-                    <View style = {styles.card}>
-                        <View style ={styles.view2}>
-                            <Image style ={styles.image2} source = {item.image} />
-                        </View>
-                        <View>
-                            <Text style ={styles.title}>{item.name}</Text>
-                        </View>
-                    </View>
-                )}
-            />
-        </View>
-
-        <View style = {styles.view3}>
-            <Text style = {styles.text3}>Where To?</Text>
-            <View style = {styles.view4}>
-                <Icon type = "material-community"
-                    name = "clock"
-                    color = {colors.grey1}
-                    size = {26}  
-                />
-                <Text style = {{marginLeft:5}}>Now</Text>
-                <Icon type = "material-community"
-                  name = "chevron-down"
-                  color = {colors.white}
-                  size = {40}  
-                />
-            </View>
-        </View>
-
-        <View style = {styles.view5}>
-            <View style = {styles.view6}>
-                <View style = {styles.view7}>
-                    <Icon type = "material-community"
-                    name = "map-marker"
-                    color = {colors.black}
-                    size = {22}  
-                    />
-                </View>
-
-                <View>
-                    <Text style = {{fontSize:18, color:colors.black}}>9945 Barker Cypress Rd Suite 123,</Text>
-                    <Text style = {{color:colors.grey3}}>Cypress, TX 77433</Text>
-                </View>
-
-                <View>
-                    <Icon type = "material-community"
-                        name = "chevron-right"
-                        color = {colors.grey}
-                        size = {26}  
-                        />
-                </View>
-            </View>
-        </View>
-
-        <View style = {{...styles.view5, borderBottomWidth:0}}>
-            <View style = {styles.view6}>
-                <View style = {styles.view7}>
-                    <Icon type = "material-community"
-                    name = "map-marker"
-                    color = {colors.black}
-                    size = {22}  
-                    />
-                </View>
-
-                <View>
-                    <Text style = {{fontSize:18, color:colors.black}}>9945 Barker Cypress Rd Suite 123,</Text>
-                    <Text style = {{color:colors.grey3}}>Cypress, TX 77433</Text>
-                </View>
-                
-                <View>
-                    <Icon type = "material-community"
-                        name = "chevron-right"
-                        color = {colors.grey}
-                        size = {26}  
-                        />
-                </View>
-            </View>
-        </View>
-
-        <Text style ={styles.text4}>Around You</Text>
-
-        <View style = {{alignItems:"center", justifyContent:"center"}}>
-            <MapView
-                ref = {_map}
-                provider = {PROVIDER_GOOGLE}
-                style = {styles.map}
-                customMapStyle = {mapStyle}
-                showsUserLocation = {true}
-                followsUserLocation = {true}
-                rotateEnabled = {true}
-                zoomEnabled = {true}
-                toolbarEnabled = {true}
-            >
-            </MapView>
-        </View>
-      </ScrollView>
-      <StatusBar style = "light" backgroundColor = "#2058c0" translucent = {true} />
-    </View>
-  )
-}
-
-export default HomePage
-
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:colors.white,
-        paddingBottom:30,
-        paddingTop:parameters.statusBarHeight
-    },
-    header:{
-      backgroundColor:colors.blue,
-      height:parameters.headerHeight,
-      alignItems:"flex-start"
-     
-    },
-    
-    image1:{
-     
-      height:100,
-      width:100,
-    
-    },
-    
-    image2:{height:60,width:60,
-            borderRadius:30,
-          },
-    
-    home:{
-     backgroundColor:colors.blue,
-     paddingLeft:20,
-     
-    },
-    
-    text1:{
-     color:colors.white,
-     fontSize:21,
-     paddingBottom:20,
-     paddingTop:20
-    },
-    
-    text2:{
-     color:colors.white,
-     fontSize:16
-    },
-    
-    view1:{
-     flexDirection:"row",
-     flex:1,
-     paddingTop:30
-    },
-    
-    button1:{
-      height:40,
-      width:150,
-      backgroundColor:colors.black,
-      borderRadius:20,
-      alignItems:"center",
-      justifyContent:"center",
-      marginTop:20
-    },
-    
-    button1Text:{
-     color:colors.white,
-     fontSize:17,
-     marginTop:-2
-    
-    },
-    card:{
-     alignItems:"center",
-     margin:22
-    
-    },
-    
-    view2:{marginBottom:5,
-          borderRadius:15,
-          backgroundColor:colors.grey6
-        },
-    
-        title:{
-          color:colors.black,
-          fontSize:16
-        },
-    view3:{flexDirection:"row",
-             marginTop :5,
-             height:50,
-             backgroundColor:colors.grey6,
-             alignItems:"center",
-             justifyContent:"space-between",
-            marginHorizontal:15
-            
-             },
-    text3:{marginLeft:15,
-            fontSize:20,
-            color:colors.black
-      },
-    
-    view4:{ flexDirection:"row",
-            alignItems:"center",
-            marginRight:15,
-            backgroundColor:"white",
-            paddingHorizontal:10,
-            paddingVertical:2,
-            borderRadius:20
+        <GooglePlacesAutocomplete
+          placeholder="Where From?"
+          styles={{
+            container: {
+              flex: 0,
             },
-    
-    view5:{ flexDirection:"row",
-    alignItems:"center",
-    backgroundColor:"white",
-    paddingVertical:25,
-    justifyContent:"space-between",
-    marginHorizontal:15,
-    borderBottomColor:colors.grey4,
-    borderBottomWidth:1,
-    flex:1
-    },
-    
-    view6:{
-    
-    
-    alignItems:"center",
-    flex:5,
-    flexDirection:"row"
-    },
-    view7:{
-    backgroundColor:colors.grey6,
-    height:40,
-    width:40,
-    borderRadius:20,
-    alignItems:"center",
-    justifyContent:"center",
-    marginRight:20
-    
-    },
-    
-    map:{
-       
-    height: 150,
-     marginVertical: 0,
-     width:SCREEN_WIDTH*0.92
-    },
-    
-    text4:{ fontSize:20,
-          color:colors.black,
-          marginLeft:20,
-          marginBottom:20
-        },
-    
-    icon1:  {marginLeft:10,
-           marginTop:5
-          },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          fetchDetails={true}
+          enablePoweredByContainer={false}
+          returnKeyType={"search"}
+          minLength={2}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
 
-    view8: {flex:4,
-          marginTop:-25
-        } ,
-    carsAround: {
-    width: 28,
-    height: 14,
-    
-    }, 
-    
-    location: {
-      width: 16,
-      height: 16,
-      borderRadius:8,
-      backgroundColor:colors.blue,
-      alignItems:"center",
-      justifyContent:"center"
-      
-      }, 
-      
-    view9:{width:4,
-    height:4,
-    borderRadius:2,
-    backgroundColor:"white"
-    }
-})
+            dispatch(setDestination(null));
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+
+        <NavOptions />
+        <NavFavourites />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomePage;
